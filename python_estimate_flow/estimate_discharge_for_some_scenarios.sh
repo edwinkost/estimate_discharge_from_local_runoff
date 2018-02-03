3d0a3c7a7193e834606d59d05a53c1470ce8d223
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2018-02-03 15:02:51 +0100

    Finalizing steps for Diede data.

--- python_estimate_flow/estimate_discharge_for_some_scenarios.sh
+++ python_estimate_flow/estimate_discharge_for_some_scenarios.sh
@@ -4,10 +4,10 @@ set - x
 python estimate_discharge_from_local_runoff.py /scratch-shared/edwinvua/data_for_diede/netcdf_process/climatology_average/ /scratch-shared/edwinvua/data_for_diede/runoff_scenarios/climatology_average_totalRunoff_monthTot_output_1979-2015.nc 2015-01-31 2015-12-31 &
 
 # year 2003 (dry year)
-python estimate_discharge_from_local_runoff.py /scratch-shared/edwinvua/data_for_diede/netcdf_process/year_2003/ /scratch-shared/edwinvua/data_for_diede/runoff_scenarios/totalRunoff_monthTot_output_2003.nc 2003-01-31 2003-12-31 &
+python estimate_discharge_from_local_runoff.py /scratch-shared/edwinvua/data_for_diede/netcdf_process/2003/ /scratch-shared/edwinvua/data_for_diede/runoff_scenarios/totalRunoff_monthTot_output_2003.nc 2003-01-31 2003-12-31 &
 
 # year 2013 (wet year)
-python estimate_discharge_from_local_runoff.py /scratch-shared/edwinvua/data_for_diede/netcdf_process/year_2013/ /scratch-shared/edwinvua/data_for_diede/runoff_scenarios/totalRunoff_monthTot_output_2013.nc 2013-01-31 2013-12-31 &
+python estimate_discharge_from_local_runoff.py /scratch-shared/edwinvua/data_for_diede/netcdf_process/2013/ /scratch-shared/edwinvua/data_for_diede/runoff_scenarios/totalRunoff_monthTot_output_2013.nc 2013-01-31 2013-12-31 &
 
 # climatology percentile 20 1979-2015
 python estimate_discharge_from_local_runoff.py /scratch-shared/edwinvua/data_for_diede/netcdf_process/climatology_percentile20/ /scratch-shared/edwinvua/data_for_diede/runoff_scenarios/climatology_percentile20_totalRunoff_monthTot_output_1979-2015.nc 2015-01-31 2015-12-31 &
